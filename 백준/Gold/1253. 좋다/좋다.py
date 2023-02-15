223a4c36f8af197360df71be87620d8c8762ecb7
Hwon-J <122521780+Hwon-J@users.noreply.github.com>
2023-02-15 21:00:53 +0900

    [Gold IV] Title: 좋다, Time: 160 ms, Memory: 115136 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -0,0 +1,22 @@
+n=int(input())
+lst=list(map(int, input().split()))
+lst.sort()
+rlt=0
+for k in range(n):
+    find=lst[k]
+    i=0
+    j=n-1
+    while i < j:
+        if lst[i]+lst[j]==find:
+            if i!=k and j!=k:
+                rlt+=1
+                break
+            elif i==k:
+                i+=1
+            elif j==k:
+                j-=1
+        elif lst[i]+lst[j]<find:
+            i+=1
+        else:
+            j-=1
+print(rlt)
