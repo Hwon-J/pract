a9f41a2c1ec78b50ecd3b8eee6d317d5ca25718b
Hwon-J <122521780+Hwon-J@users.noreply.github.com>
2023-03-02 14:01:24 +0900

    [Bronze I] Title: 딱지놀이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/14696. 딱지놀이/딱지놀이.py
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.py
@@ -1,24 +1,18 @@
-# N: 딱지게임 라운드 수
 N=int(input())
 for i in range(N):
     a=list(map(int, input().split()))
     b=list(map(int, input().split()))
-    alst=[0]*5
-    blst=[0]*5
-    for j in a:
-        alst[j]+=1
-    for k in b:
-        blst[k]+=1
-    cnt = 4
+
+    cnt = 5
     while cnt>=0:
-        if alst[cnt]==blst[cnt]:
+        if a.count(cnt)==b.count(cnt):
             cnt-=1
             if cnt==0:
                 print('D')
                 break
-        elif alst[cnt]>blst[cnt]:
+        elif a.count(cnt)>b.count(cnt):
             print('A')
             break
-        elif alst[cnt]<blst[cnt]:
+        elif a.count(cnt)<b.count(cnt):
             print('B')
             break
