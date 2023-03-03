47839332ca3e177f5d49cdaedbfefc1d1c461644
Hwon-J <122521780+Hwon-J@users.noreply.github.com>
2023-03-03 10:26:40 +0900

    [Bronze I] Title: 색종이, Time: 1320 ms, Memory: 38436 KB -BaekjoonHub

---
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -0,0 +1,17 @@
+# N:색종이의 장수
+N=int(input())
+paper=[[0]*1001 for _ in range(1001)]
+for k in range(N):
+    # (a,b)
+    a,b,w,h=map(int, input().split())
+    for i in range(a,a+w):
+        for j in range(b,b+h):
+            paper[j][i]=k+1
+
+for l in range(N):
+    cnt=0
+    for m in range(1001):
+        for n in range(1001):
+            if paper[n][m]==l+1:
+                cnt+=1
+    print(cnt)
