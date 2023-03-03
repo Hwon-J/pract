f8c37799df5e293aa6737b19a93fe365e75ed8cc
Hwon-J <122521780+Hwon-J@users.noreply.github.com>
2023-03-03 11:19:39 +0900

    [Bronze I] Title: 색종이, Time: 68 ms, Memory: 38304 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -4,18 +4,19 @@ paper=[[0]*1001 for _ in range(1001)]
 mn=21e8
 mx=0
 for k in range(N):
+    # (a,b)
     a,b,w,h=map(int, input().split())
     for i in range(a,a+w):
         for j in range(b,b+h):
-            if i<0 or i>1001 or j<0 or j>1001: continue
             paper[j][i]=k+1
-    if mx < max(a + w, b + h):
+    if mx< max(a+w,b+h):
         mx = max(a + w, b + h)
-    if mn > min(a, b):
+    if mn>min(a,b):
         mn = min(a, b)
-
-lst=[0]*(N+1)
-for i in range(1,N+1):
-    for k in paper:
-        lst[i]+=k.count(i)
-    print(lst[i])
+for l in range(N):
+    cnt=0
+    for m in range(mn,mx+1):
+        for n in range(mn,mx+1):
+            if paper[n][m]==l+1:
+                cnt+=1
+    print(cnt)
