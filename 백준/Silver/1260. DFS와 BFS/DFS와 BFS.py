df8cd67ae2e281b3c573710462248cfc105b6713
Hwon-J <122521780+Hwon-J@users.noreply.github.com>
2023-03-27 14:05:47 +0900

    [Silver II] Title: DFS와 BFS, Time: 472 ms, Memory: 34168 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,22 +1,15 @@
 from collections import deque
-N, M, V = map(int, input().split())
-graph = [[] for _ in range(N + 1)]
-visited = [False for _ in range(N + 1)]
-visited2 = [False for _ in range(N + 1)]
-for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
 
-def dfs(level, num):
+# 정점의 개수만큼
+def dfs(num):
     print(num, end=' ')
-    if level == N:
+    if sum(visited)==N:
         return
 
     for i in sorted(graph[num]):
         if visited[i] == False:
             visited[i] = True
-            dfs(level + 1, i)
+            dfs(i)
 
 def bfs(num):
     q=deque()
@@ -30,7 +23,25 @@ def bfs(num):
                 q.append(i)
                 visited2[i]=True
 
+
+
+
+
+# N: 정점의 개수, M: 간선의 개수, V: 탐색을 시작할 번호
+N, M, V = map(int, input().split())
+# 인접리스트를 위한 빈리스트 만들기
+graph = [[] for _ in range(N + 1)]
+# DFS의 방문체크
+visited = [False for _ in range(N + 1)]
+# Bfs의 방문체크
+visited2 = [False for _ in range(N + 1)]
+# 인접리스트 채우기
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
 visited[V]=visited2[V]= True
-dfs(1, V)
+dfs(V)
 print()
 bfs(V)
