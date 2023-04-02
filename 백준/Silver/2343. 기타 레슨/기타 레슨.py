b051c30cd19ab08b288f22a2eedf16feaba41caf
Hwon-J <122521780+Hwon-J@users.noreply.github.com>
2023-04-02 11:14:55 +0900

    [Silver I] Title: 기타 레슨, Time: 472 ms, Memory: 42340 KB -BaekjoonHub

---
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.py
@@ -0,0 +1,20 @@
+# N: 강의의 수, M : 블루레이 수
+N, M = map(int, input().split())
+lst = list(map(int, input().split()))
+st,ed = max(lst),sum(lst)
+while 1:
+    mid = (st+ed)//2
+    cnt,summ=0,0
+    for i in lst:
+        if summ + i > mid:
+            cnt+=1
+            summ=0
+        summ+=i
+    if cnt >= M:
+        st = mid+1
+    else:
+        ed=mid
+
+    if st>=ed:
+        break
+print(st)
