dcc713d14e7d28b3ad9c6d0f54c54081653753d6
Hwon-J <122521780+Hwon-J@users.noreply.github.com>
2023-04-09 00:01:27 +0900

    [Bronze III] Title: 직각삼각형, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,10 @@
+while 1:
+    lst = list(map(int, input().split()))
+    lst.sort()
+    if sum(lst)==0:
+        break
+    else:
+        if lst[0]**2+lst[1]**2==lst[2]**2:
+            print('right')
+        else:
+            print('wrong')
