8ae23df8d395706cc7ef325a0461e3a359c5d32f
Hwon-J <122521780+Hwon-J@users.noreply.github.com>
2023-05-11 20:37:14 +0900

    [Gold V] Title: 최소비용 구하기, Time: 348 ms, Memory: 60580 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -7,7 +7,6 @@ M = int(input())
 graph = [[] for _ in range(N+1)]
 INF = int(21e8)
 rlt = [INF]*(N+1)
-visit = [0]*(N+1)
 for _ in range(M):
     a, b, c = map(int, input().split())
     graph[a].append([b,c])
@@ -23,7 +22,7 @@ def dijk(st):
         if rlt[now] < dist: continue
         for i in graph[now]:
             tm = dist + i[1]
-            if tm < rlt[i[0]] and not visit[i[0]]:
+            if tm < rlt[i[0]]:
                 rlt[i[0]] = tm
                 heapq.heappush(q, [tm, i[0]])
 
