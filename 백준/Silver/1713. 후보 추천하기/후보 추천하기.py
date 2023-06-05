5a4ffa8d750edcbf911b9a2a669696be24330efc
Hwon-J <122521780+Hwon-J@users.noreply.github.com>
2023-06-05 14:32:39 +0900

    [Silver I] Title: 후보 추천하기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1713. 후보 추천하기/후보 추천하기.py
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.py
@@ -5,14 +5,12 @@ arr = list(map(int, input().split()))
 result = []
 
 for i in arr:
-    found = False
-
     if i not in [x[0] for x in result]:
         if len(result) < N:
             result.append([i, 1])
         else:
-            min_count = min(result, key=lambda x: x[1])
-            result.remove(min_count)
+            mn = min(result, key=lambda x: x[1])
+            result.remove(mn)
             result.append([i, 1])
     else:
         for j in range(len(result)):
