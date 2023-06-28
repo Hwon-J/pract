b231363996ca123cf50dc8eeb4ce35c5a83df29c
Hwon-J <122521780+Hwon-J@users.noreply.github.com>
2023-06-28 09:16:05 +0900

    [Silver I] Title: 숨바꼭질, Time: 104 ms, Memory: 35108 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+def bfs(n, k):
+    q = deque()
+    q.append(n)
+
+    while q:
+        x = q.popleft()
+
+        if x == k:
+            return visit[x]
+
+        for i in (x-1, x+1, 2*x):
+            if 0 <= i <= 100000 and visit[i]==0:
+                visit[i] = visit[x] + 1
+                q.append(i)
+
+
+n, k = map(int, input().split())
+visit = [0] * 100001
+
+print(bfs(n, k))
