a919bc442e727d7d288eac428dd6ff6871c2def1
Hwon-J <122521780+Hwon-J@users.noreply.github.com>
2023-07-06 21:22:44 +0900

    [Gold V] Title: 간선 이어가기 2, Time: 316 ms, Memory: 55852 KB -BaekjoonHub

---
+++ 백준/Gold/14284. 간선 이어가기 2/간선 이어가기 2.py
@@ -0,0 +1,31 @@
+import sys, heapq
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a,b,c = map(int, input().split())
+    graph[a].append([b,c])
+    graph[b].append([a,c])
+
+s, t = map(int, input().split())
+INF = int(21e8)
+dist = [INF]*(n+1)
+dist[s] = 0
+visit = [0]*(n+1)
+
+def dijk(s):
+    q = []
+    heapq.heappush(q, (0, s))
+    while q:
+        cost, node = heapq.heappop(q)
+        if dist[node] < cost: continue
+        for i in graph[node]:
+            if cost + i[1] < dist[i[0]]:
+                dist[i[0]] = cost + i[1]
+                heapq.heappush(q, (cost + i[1], i[0]))
+
+dijk(s)
+print(dist[t])
