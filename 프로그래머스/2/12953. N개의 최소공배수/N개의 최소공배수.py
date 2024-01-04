4d1ff95029c4cf6d7c4af98683c7594ae82f4c96
Hwon-J <122521780+Hwon-J@users.noreply.github.com>
2024-01-04 19:23:47 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,20 +1,10 @@
+import math
+
+def lcm(a,b):
+    return a*b // math.gcd(a,b)
+
 def solution(arr):
-    answer = 1
-    temp = [0]*101
-    for num in arr:
-        tm = 2
-        cnt = 0
-        while 1:
-            if num == 1:
-                break
-            if num % tm == 0:
-                cnt+=1
-                num = num // tm
-                temp[tm] = max(temp[tm], cnt)
-            else:
-                cnt = 0
-                tm+=1
-    for idx in range(1, 101):
-        if temp[idx] != 0:
-            answer *= idx**temp[idx]
+    answer = arr[0]
+    for i in arr[1:]:
+        answer = lcm(answer,i)
     return answer
