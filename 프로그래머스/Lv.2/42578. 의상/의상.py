e3966b3f2db302ff848fbc6b35a6f11dcba6e03f
Hwon-J <122521780+Hwon-J@users.noreply.github.com>
2024-01-14 21:18:59 +0900

    [level Lv.2] Title: 의상, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Lv.2/42578. 의상/의상.py
+++ 프로그래머스/Lv.2/42578. 의상/의상.py
@@ -1,11 +1,6 @@
 def solution(clothes):
-    answer = 1
-    cloth_cnt = {}
-    for cloth in clothes:
-        if cloth[1] in cloth_cnt:
-            cloth_cnt[cloth[1]] += 1
-        else:
-            cloth_cnt[cloth[1]] = 1
-    for value in cloth_cnt.values():
-        answer *= value+1
-    return answer - 1
+    from collections import Counter
+    from functools import reduce
+    cnt = Counter([kind for name, kind in clothes])
+    answer = reduce(lambda x, y: x*(y+1), cnt.values(), 1) - 1
+    return answer
