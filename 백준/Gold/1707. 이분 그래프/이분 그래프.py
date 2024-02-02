a44ea195d443536363c5733b7d92caf5fe6c93e4
Hwon-J <122521780+Hwon-J@users.noreply.github.com>
2024-02-02 23:05:01 +0900

    [Gold IV] Title: 이분 그래프, Time: 1252 ms, Memory: 53208 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,39 +1,33 @@
-from collections import deque
 import sys
+sys.setrecursionlimit(100000)
 input = sys.stdin.readline
 
 T = int(input())
+
+def dfs(node, group):
+    global flag
+    visit[node] = group
+    for neighbor in arr[node]:
+        if visit[neighbor] == 0:
+            dfs(neighbor, 3 - group)
+        elif visit[node] == visit[neighbor]:
+            flag = 0
+            return
+
 for _ in range(T):
     V, E = map(int, input().split())
-    arr = [[] for _ in range(V+1)]
-    visit = [0]*(V+1)
-    # 인접리스트 구하기
+    arr = [[] for _ in range(V + 1)]
+    visit = [0] * (V + 1)
+    flag = 1
+
     for _ in range(E):
         a, b = map(int, input().split())
         arr[a].append(b)
         arr[b].append(a)
-    flag = 1
-    def bfs(st):
-        global flag
-        q= deque()
-        q.append(st)
-        visit[st]=1
-        while q:
-            now = q.popleft()
-            for i in arr[now]:
-                if visit[i]==0:
-                    q.append(i)
-                    if visit[now]==1:
-                        visit[i]=2
-                    elif visit[now]==2:
-                        visit[i]=1
-                elif visit[now]==visit[i]:
-                    flag=0
 
-
-    for i in range(1, V+1):
-        if visit[i]==0:
-            bfs(i)
+    for i in range(1, V + 1):
+        if visit[i] == 0 and flag:
+            dfs(i, 1)
 
     if flag:
         print('YES')
