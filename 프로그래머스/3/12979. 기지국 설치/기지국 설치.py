d434331e9a7613301425b5b60727f33b4b2f0fda
Hwon-J <122521780+Hwon-J@users.noreply.github.com>
2024-02-11 15:13:21 +0900

    [level 3] Title: 기지국 설치, Time: 8.15 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -1,8 +1,19 @@
-def solution(n, arr, w):
-    bef=-w
-    cnt=0
-    ww=w*2+1
-    for x in arr:
-        cnt+=(x-bef-1)//ww
-        bef=x
-    return cnt+(n+w-bef)//ww
+import math
+
+def solution(n, stations, w):
+    answer = 0
+    able = []
+
+    for station in stations:
+        st = max(1, station - w)
+        ed = min(n, station + w)
+        able.append([st, ed])
+
+    answer += math.ceil((able[0][0]-1)/(w*2+1))
+    answer += math.ceil((n-able[-1][1])/(w*2+1))
+    for i in range(len(able)-1):
+        tm = able[i+1][0] - able[i][1]-1
+        answer += math.ceil(tm/(w*2+1))
+    return answer
+
+
