a7df3a74cc67bdc06c338d79bf3495c06b9ccc89
Hwon-J <122521780+Hwon-J@users.noreply.github.com>
2024-02-15 22:17:38 +0900

    [level 2] Title: 피로도, Time: 0.08 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,18 +1,17 @@
 answer = 0  
-def DFS(k, cnt, dungeons, visited):
+def DFS(k, cnt, dungeons, visit):
     global answer
     if answer < cnt:  
         answer = cnt
     
     for i in range(len(dungeons)):
-        if visited[i] == 0 and k >= dungeons[i][0]:
-            visited[i] = 1  
-            DFS(k - dungeons[i][1], cnt + 1, dungeons, visited)
-            visited[i] = 0  
+        if visit[i] == 0 and k >= dungeons[i][0]:
+            visit[i] = 1  
+            DFS(k - dungeons[i][1], cnt + 1, dungeons, visit)
+            visit[i] = 0  
 
 def solution(k, dungeons):
     global answer
-    answer = 0
-    visited = [0] * len(dungeons)
-    DFS(k, 0, dungeons, visited)
+    visit = [0] * len(dungeons)
+    DFS(k, 0, dungeons, visit)
     return answer
