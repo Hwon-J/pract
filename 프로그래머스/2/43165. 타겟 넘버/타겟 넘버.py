036dcf52f59995d4a45581f5680b8c2144ed884b
Hwon-J <122521780+Hwon-J@users.noreply.github.com>
2024-02-19 17:53:21 +0900

    [level 2] Title: 타겟 넘버, Time: 2.78 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,5 +1,14 @@
-from itertools import product
+answer = 0
 def solution(numbers, target):
-    l = [(x, -x) for x in numbers]
-    s = list(map(sum, product(*l)))
-    return s.count(target)
+    global answer
+    def dfs(level, summ):
+        global answer
+        if level == len(numbers):
+            if summ == target:
+                answer += 1
+            return
+        dfs(level + 1, summ + numbers[level])
+        dfs(level + 1, summ - numbers[level])
+
+    dfs(0, 0)
+    return answer
