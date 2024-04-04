8f2dae8b2c56d333082e8551c5e9dd3f491f9680
Hwon-J <122521780+Hwon-J@users.noreply.github.com>
2024-04-04 15:00:56 +0900

    [Gold V] Title: 동전 2, Time: 380 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -0,0 +1,12 @@
+n, k = map(int, input().split())
+coins = [int(input()) for _ in range(n)]
+INF = int(21e8)
+dp = [INF]*(k+1)
+dp[0] = 0
+for coin in coins:
+    for i in range(coin, k+1):
+        dp[i] = min(dp[i], dp[i-coin]+1)
+if dp[k] == INF:
+    print(-1)
+else:
+    print(dp[k])
